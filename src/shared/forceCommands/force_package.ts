c8cbe8b0469eb1d1e88ded17e9d490de86c622bb
John M. Daniel <ImJohnMDaniel@ce-v.com>
2019-04-28 22:42:02 -0400

    minor updates

--- src/shared/forceCommands/force_package.ts
+++ src/shared/forceCommands/force_package.ts
@@ -1,11 +1,24 @@
 import { UX } from '@salesforce/command';
 import { Org } from '@salesforce/core';
-import child_process = require('child_process');
 import * as _ from 'lodash';
-import util = require('util');
 import { PackageVersion } from '../../types/package_version';
+// tslint:disable-next-line:no-var-requires
+const {onExit, chunksToLinesAsync, chomp} = require('@rauschma/stringio');
+// tslint:disable-next-line:no-var-requires
+const {spawn} = require('child_process');
 
-const exec = util.promisify(child_process.exec);
+// tslint:disable-next-line:no-var-requires
+// const spawn = require('child-process-promise').spawn;
+
+async function echoReadable(readable) {
+    let commandJSONOutput = '';
+    for await (const line of chunksToLinesAsync(readable)) {
+      // console.log('LINE: ' + chomp(line) );
+      commandJSONOutput = commandJSONOutput + chomp(line).trim();
+    }
+    console.log(commandJSONOutput);
+    return JSON.parse(commandJSONOutput);
+  }
 
 export async function retrievePackagesCurrentlyInstalled( thisOrg: Org, thisUx: UX ) {
 
@@ -15,7 +28,6 @@ export async function retrievePackagesCurrentlyInstalled( thisOrg: Org, thisUx: 
     const args = [];
 
     // base command
-    args.push('sfdx');
     args.push('force:package:installed:list');
 
     // USERNAME argument
@@ -25,14 +37,107 @@ export async function retrievePackagesCurrentlyInstalled( thisOrg: Org, thisUx: 
     // have the output returned as JSON
     args.push('--json');
 
-    const installedListCallResult = await exec(args.join(' '));
+    // const stdioValue = 'inherit'; // this will show the output from the install command but does not appear to be captured to the stdout
+    // const stdioValue = 'pipe'; // this stdio value appears to work
+    const stdioValue = ['ignore', 'pipe', process.stderr]; // this goes with the echoReadable option
+    // const stdioValue = [process.stdin, process.stdout, process.stderr];
+
+    // const promise = await spawn('sfdx', args, { stdio: stdioValue});
+    const childProcess = spawn('sfdx', args, { stdio: stdioValue});
+
+    // const source = spawn('cat', [filePath],
+    // {stdio: ['ignore', 'pipe', process.stderr]});
+    const installedPackageListJson = await echoReadable(childProcess.stdout);
+
+    console.log( installedPackageListJson );
+
+    if ( installedPackageListJson.status !== 0 ) {
+        throw Error('problems retrieving installed package list' + installedPackageListJson);
+    }
+
+    // await onExit(childProcess);
+    // childProcess.stdout.on('data', data => {
+    //   console.log(`installedListCallChildProcess stdout: ${data}`);
+    //   // installedPackageListJson = JSON.parse(data);
+    //   // console.log(data);
+    // });
+
+    // console.log(process.stdout);
+    // installedPackageListJson = JSON.parse(process.stdout);
+
+    // const installedListCallChildProcess = promise.childProcess;
+
+    // console.log('[spawn] installedListCallChildProcess.pid: ', installedListCallChildProcess.pid);
+    // console.log('[spawn] installedListCallChildProcess.stdout: ', installedListCallChildProcess.stdout);
+
+    // let installedPackageListJson = { };
+    // console.log('before installedListCallChildProcess.stdout.on(data');
+    // installedListCallChildProcess.stdout.on('data', data => {
+    //   console.log(`installedListCallChildProcess stdout: ${data}`);
+    //   installedPackageListJson = JSON.parse(data);
+    //   // console.log(data);
+    // });
+    // console.log('before installedListCallChildProcess.stderr.on(data');
+    // installedListCallChildProcess.stderr.on('data', data => {
+    //   console.log(`installedListCallChildProcess stderr: ${data}`);
+    //   // console.log(data);
+    // });
+
+    // promise.then(() => {
+    //     console.log('[spawn] done!');
+    //     console.log(' ');
+    //     console.log(' ');
+    //     console.log('________________________________________________________________________________');
+    //     console.log(' ');
+    //     console.log(promise);
+    //     console.log(' ');
+    //     console.log('________________________________________________________________________________');
+    // })
+    // .catch(err => {
+    //     console.error('[spawn] ERROR: ', err);
+    //     this.exit();
+    // });
+
+    // this.exit();
+    // console.log(args);
+
+    // console.log('before promise await______________________________________________________________________________');
+    // await installedListCallChildProcess;
+    // console.log('after promise await______________________________________________________________________________');
+
+    // console.log(PackageInstalledListCommand.run(args));
+
+    // const installedListCallResult =  await PackageInstalledListCommand.run(args);
+    // const installedListCallPromise =  PackageInstalledListCommand.run(args);
+    // installedListCallPromise.then(onfulfilled => {
+    //     console.log('inside the promise then for onfulfilled');
+    //     console.log(onfulfilled); // "Stuff worked!"
+    //   },
+    //   onrejected => {
+    //     console.log('inside the promise then for rejected');
+    //     console.log(onrejected);
+    //   }
+    // );
+    // console.log('after the promise then');
+    // console.log('before promise await');
+    // await installedListCallPromise;
+    // console.log('after promise await');
+    console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+//    console.log(installedListCallPromise);
+//    console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
     // console.log(installedListCallResult.stdout);
+    // console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
     // const installedPackageListJson = JSON.parse(installedListCallResult.stdout);
 
     thisUx.stopSpinner();
+    // this.exit();
+
+    return installedPackageListJson.result as PackageVersion[];
 
     // return JSON.parse(installedListCallResult.stdout).result as PackageVersions;
-    return JSON.parse(installedListCallResult.stdout).result as PackageVersion[];
+//    return JSON.parse(installedListCallResult.stdout).result as PackageVersion[];
+
+    // return {} as PackageVersion[];
 
     // console.log(installedPackageListJson.result);
     // const thePackageVersionList = installedPackageListJson.result as PackageVersion[];
